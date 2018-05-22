9d3959d38e8b475f0bd28ac989e81d689d28e76d
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-22 13:36:53 +0200

    Maj Réponse si param non renseigné / tous renseignés

--- src/main/java/dev/sgp/web/EditerCollaborateursController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateursController.java
@@ -13,17 +13,44 @@ public class EditerCollaborateursController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
 		String matricule = req.getParameter("matricule");
+		String titre = req.getParameter("titre");
+		String nom = req.getParameter("nom");
+		String prenom = req.getParameter("prenom");
 		
-		if(matricule == null)
+		if(matricule == null || titre == null || nom == null || prenom == null)
 		{
 			resp.setStatus(400);
 			resp.getWriter().write("<h1>Code=" + resp.getStatus() + "</h1>");
-			resp.getWriter().write("Un matricule est attendu");
+			resp.getWriter().write("Les paramètres suivants sont incorects:");
+			
+			if(matricule == null)
+			{
+				resp.getWriter().write(" matricule");
+			}
+			
+			if(titre == null)
+			{
+				resp.getWriter().write(" titre");
+			}
+			
+			if(nom == null)
+			{
+				resp.getWriter().write(" nom");
+			}
+			
+			if(prenom == null)
+			{
+				resp.getWriter().write(" prenom");
+			}
 		}
 		else
 		{
 			resp.getWriter().write("<h1>Code=" + resp.getStatus() + "</h1>");
-			resp.getWriter().write("Matricule : " + matricule);
+			resp.getWriter().write("Création d'un collaborateur avec les informations suivantes : ");
+			resp.getWriter().write("Matricule : " + matricule + ",");
+			resp.getWriter().write("Titre : " + titre + ",");
+			resp.getWriter().write("Nom : " + nom + ",");
+			resp.getWriter().write("Prénom : " + prenom);
 		}
 	}
 }
