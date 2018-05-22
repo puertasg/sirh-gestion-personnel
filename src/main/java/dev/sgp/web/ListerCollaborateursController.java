3412cb430fe1bd424c3f925e160dd5af24ec2cc3
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-22 12:01:35 +0200

    Implémentation ListerCollaborateursController

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,5 +1,16 @@
 package dev.sgp.web;
 
-public class ListerCollaborateursController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListerCollaborateursController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		resp.getWriter().write("Hello ListerCollaborateursController");
+	}
 }
