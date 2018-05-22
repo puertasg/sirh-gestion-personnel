2ddc560c38ec9064687e549006eab90d70c4573b
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-22 14:32:24 +0200

    Passage en doPost

--- src/main/java/dev/sgp/web/EditerCollaborateursController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateursController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class EditerCollaborateursController extends HttpServlet {
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
 		String matricule = req.getParameter("matricule");
 		String titre = req.getParameter("titre");
