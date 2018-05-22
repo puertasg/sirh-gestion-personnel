2f9d16b55bcd2cd51adbc08a9386467c56d2dec9
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-22 12:48:00 +0200

    Revert "Ajout message d'erreur si pas de matricule"
    This reverts commit ae07555a56529ef63bb5a8a83348c7fc3aeba5cb.

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,28 +1,5 @@
 package dev.sgp.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class EditerCollaborateurController extends HttpServlet {
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-	{
-		String matricule = req.getParameter("matricule");
-		if(matricule == null)
-		{
-			resp.setStatus(400);
-			resp.getWriter().write("Un matricule est attendu");
-		}
-		else
-		{
-			resp.getWriter().write("<h1>Edition de collaborateur</h1>"
-			+ "Matricule : " + matricule);
-		}
-	}
+public class EditerCollaborateurController {
 
 }
