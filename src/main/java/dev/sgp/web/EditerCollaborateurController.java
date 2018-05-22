7f50521616a47b3572e5e0870a5e2dc5510e7466
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-22 12:56:08 +0200

    Implémentation EditerCollaborateursController

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,5 +1,23 @@
 package dev.sgp.web;
 
-public class EditerCollaborateurController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditerCollaborateurController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		String matricule = req.getParameter("matricule");
+		
+		if(matricule == null)
+		{
+			resp.setStatus(400);
+			resp.getWriter().write("Un matricule est attendu");
+		}
+	}
 }
